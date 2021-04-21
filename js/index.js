39be7b28ac79eeab6a6f333ef83c55b2d108d76f
Svet-lik <svet-st@yandex.ru>
2021-04-21 15:17:31 +0300

    day1

--- js/index.js
+++ js/index.js
@@ -3,16 +3,16 @@ const gloAcademyList = document.querySelector('.glo-academy-list');
 const trendingList = document.querySelector('.trending-list');
 const musicList = document.querySelector('.music-list');
 
-const creatCard = (dataVideo) => {
-  
+// одна карточка с видео
+const creatCard = (dataVideo) => {  
   const imgUrl = dataVideo.snippet.thumbnails.high.url;
   const videoId = dataVideo.id.videoId || dataVideo.id;
   const titleVideo = dataVideo.snippet.title;
   const dateVideo = dataVideo.snippet.publishedAt;
   const channelTitle = dataVideo.snippet.channelTitle;
-
-  // одна карточка с видео
   const card = document.createElement('div');
+  const strViewCount = dataVideo.statistics ? `<span class="video-views">${dataVideo.statistics.viewCount} views</span>` : '';
+  
   card.classList.add('video-card');
   card.innerHTML = `
     <div class="video-thumb">
@@ -26,7 +26,8 @@ const creatCard = (dataVideo) => {
     <h3 class="video-title">${titleVideo}</h3>
     <div class="video-info">
       <span class="video-counter">
-        <span class="video-date">${new Date(dateVideo).toLocaleString()}</span>
+      ${strViewCount}	
+      <span class="video-date">${new Date(dateVideo).toLocaleString()}</span>
       </span>
       <span class="video-channel">${channelTitle}</span>
     </div>`;
